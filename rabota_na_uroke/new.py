940e0bb87fa8c6cfd73fb25245dfdbe53dc39931
MEEEXIM <maxim-malyshev.2010@mail.ru>
2024-09-15 19:49:46 +0700

    d

---
+++ rabota_na_uroke/new.py
@@ -0,0 +1,8 @@
+class trpt:
+    def __init__(self, name, V, kolvo, m):
+        self.name = name
+        self.V = V
+        self.kolvo = kolvo
+        self.m = m
+    def L(self, S):
+        return S/
